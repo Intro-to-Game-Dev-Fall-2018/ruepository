3e4f1ea1bf01d046f1fa60277529c646df722f83
Rue Laurito <cal707@nyu.edu>
2018-10-09 03:18:05 -0400

    Implemented the "restart" feature.

---
+++ ARCADEVOLLEYBALL/Assets/Scripts/RestartScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class RestartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Backspace))
+		{
+			SceneManager.LoadScene("SampleScene");
+		}
+	}
+}
