4ee4e0df8078a147cc73bec36e4353198b6e0542
Rue Laurito <cal707@nyu.edu>
2018-10-09 08:37:08 -0400

    Aesthetic changes made to the theme of "Arcade Volleybomb".

---
+++ ARCADEVOLLEYBALL/Assets/Scripts/SpriteSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteSwitcher : MonoBehaviour {
+	public Sprite[] sprites;
+
+	SpriteRenderer _spriteRenderer;
+	void Start () {
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void SwitchToSprite(int index)
+	{
+		if(index < 0 || index >= sprites.Length)
+		{
+			Debug.LogError("Attempted to display a sprite outside the valid range: " + index);
+			return;
+		}
+
+		_spriteRenderer.sprite = sprites[index];
+	}
+}
